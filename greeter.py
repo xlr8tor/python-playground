7b7cb9667bc8aedd2b727c79bf1c43889a024d8a
charles jeremiah <charlesjeremiah89@yahoo.com>
2023-01-02 20:42:53 +0100

    .

--- greeter.py
+++ greeter.py
@@ -3,6 +3,6 @@
 import argparse
 
 parser = argparse.ArgumentParser(description='Say hello')
-parser.add_argument('name', help='Name to greet')
+parser.add_argument('-n','--name', metavar='name', default='World' ,help='Name to greet')
 args = parser.parse_args()
-print('Hello' + args.name + '!')
+print('Hello, ' + args.name + '!')
--- test.py
+++ test.py
@@ -15,18 +15,27 @@ def test_exists():
 def test_running():
     """Runs using python3"""
     out = getoutput(f'python3 {prg}')
-    assert out.strip() == 'Hello world'
+    assert out.strip() == 'Hello, World!'
 
 # -------------------------
 def test_executable():
     """Says 'Hello world' by default"""
     out = getoutput(prg)
-    assert out == 'Hello world'
+    assert out == 'Hello, World!'
 
 # -------------------------
 def test_usage():
     """usage page"""
     for flag in ['-h','--help']:
-        rv, out = getstatusoutput(f'{prg} -h')
+        rv, out = getstatusoutput(f'{prg} {flag}')
         assert rv == 0
         assert out.lower().startswith('usage')
+
+# -------------------------
+def test_input():
+    """test input"""
+    for val in ['Universe', 'Multiverse']:
+        for option in ['-n', '--name']:
+            rv, out = getstatusoutput(f'{prg} {option} {val}')
+            assert rv == 0
+            assert out.strip() == f"Hello, {val}!"
