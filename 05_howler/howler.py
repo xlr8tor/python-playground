7fb1a15fab35bcd339d69e4279448392fb3e4400
charles jeremiah <charlesjeremiah89@yahoo.com>
2023-01-04 10:10:45 +0100

    howler updated

--- 05_howler/howler.py
+++ 05_howler/howler.py
@@ -9,19 +9,24 @@ def get_args():
 
     parser.add_argument('text', help='Input string or file')
 
-    parser.add_argument('-o', '--outfile', help='Output filename', default='')
+    parser.add_argument('-o', '--outfile', help='Output filename', default='', metavar='outfile')
     return parser.parse_args()
 
 def main():
     args = get_args()
     file = args.text
+    result = ''
     if os.path.isfile(file):
-        print(open(file).read().upper())
+       result = open(file).read().upper()
     else:
-        print(args.text.upper())
+        result = args.text.upper()
 
-
-
+    if args.outfile: 
+        out_fh = open(args.outfile,'wt')
+        out_fh.write(result)
+        out_fh.close()
+    else:
+        print(result)
 
 
 if __name__ == "__main__":
