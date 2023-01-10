1992722050adff3c7d152bf3fb418c4bac0f465e
charles jeremiah <charlesjeremiah89@yahoo.com>
2023-01-10 12:05:26 +0100

    main func

--- 06_word_count/wc.py
+++ 06_word_count/wc.py
@@ -21,6 +21,18 @@ def get_args():
 
 def main():
     args = get_args()
+    total_lines, total_words, total_bytes = 0, 0, 0
+    for fh in args.file:
+        num_lines, num_words, num_bytes = 0, 0, 0
+        for line in fh:
+            num_lines += 1
+            num_bytes += len(line)
+            num_words += len(line.split())
+        total_bytes += num_bytes
+        total_lines += num_lines
+        total_words += num_words
+
+    print(f'{num_lines:8}{num_words:8}{num_bytes:8} {fh.name}')
 
 
 if __name__ == "__main__":
